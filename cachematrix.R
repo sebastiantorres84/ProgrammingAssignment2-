ca032e35d8caa03be3f240b72bf36fadbd8b19ef
sebastiantorres84 <70590454+sebastiantorres84@users.noreply.github.com>
2020-09-01 12:16:44 -0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,9 +2,6 @@
 ## we create an object, an array
 ## which stores cache in its inverse and
 ## stores an array and caches its inverse.
-
-
-
 makeCacheMatrix <- function(x = matrix()){
   inver <- NULL
   set <- function(y){
@@ -16,12 +13,9 @@ makeCacheMatrix <- function(x = matrix()){
   getInverse <- function() {inver}
   list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
-
-
 ########################
 #now, we are going to calculate the inverse of the matrix created by makecachematrix
 #we recover the inverse in case it has not changed
-
 cacheSolve <- function(x, ...){
   inver <- x$getInverse()
   if(!is.null(inver)){
